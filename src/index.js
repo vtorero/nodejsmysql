4af9b26bce90472d8a20d36a6c998fd174c8e8b5
Victor Jimenez <vjimenez@wigilabs.com>
2024-05-13 00:24:50 -0500

    fist commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import app from './app.js';
 import { PORT } from './config.js';
 
 app.listen(PORT);
-console.log("Servidor ejecutando en puerto",PORT)
+console.log("Servidor ejecutando en puertoXX",PORT)
 
 
 
