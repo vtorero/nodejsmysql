db2868e2517d4578965c94066833cb78156596b6
Victor Jimenez <vjimenez@wigilabs.com>
2024-05-13 11:43:19 -0500

    fist commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import app from './app.js';
-import { PORT } from './config.js';
+import { PORT,DB_HOST,DB_DATABASE,DB_USER,DB_PASSWORD,DB_PORT } from './config.js';
 
 app.listen(PORT);
 console.log("Servidor ejecutando en...",PORT)
+console.log("DB_HOST",DB_HOST)
+console.log("DB_DATABASE",DB_DATABASE)
+console.log("DB_USER",DB_USER)
+console.log("DB_PASSWORD",DB_PASSWORD)
+console.log("DB_PORT",DB_PORT)
+
+
 
 
 
